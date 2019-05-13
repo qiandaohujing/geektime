8eb1c3d31e9cc772709fbc9aaeaea467d0817d2f
qiandaohujing <979423879@qq.com>
2019-05-13 20:05:39 +0800

    1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<PersonalInfo v-model="phoneInfo" :zip-code.sync="zipCode"/>
+		<!-- <PersonalInfo
+			:phone-info="phoneInfo"
+			:zip-code="zipCode"
+			@change="val => (phoneInfo = val)"
+			@update:zipCode="val => (zipCode = val)"
+		/>-->
+		<span>phoneInfo：</span>
+		{{ phoneInfo }}
+		<br>
+		<span>zipCode：</span>
+		{{ zipCode }}
+	</div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import PersonalInfo from "./components/PersonalInfo";
+	export default {
+		components: {
+			PersonalInfo
+		},
+		data() {
+			return {
+				phoneInfo: {
+					areaCode: "+86",
+					phone: ""
+				},
+				zipCode: ""
+			};
+		}
+	};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
